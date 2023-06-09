4c381570b2b78d909727d13b15ab2e695f7e1933
Kelven G <kelvengalvao@gmail.com>
2023-06-09 00:40:04 -0300

    Initial Commit

---
+++ futzin/futzinApp.swift
@@ -0,0 +1,17 @@
+//
+//  futzinApp.swift
+//  futzin
+//
+//  Created by Kelven Galvao on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct futzinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
